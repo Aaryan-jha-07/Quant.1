29896c6c6bb9f77ec774c46394baa3700849766c
mac <aaryanjhaixrose@gmail.com>
2026-05-04 22:04:25 +0530

    Update API URL to live Render production server

--- frontend-client/src/api/quantClient.js
+++ frontend-client/src/api/quantClient.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:8000/api';
+const API_BASE_URL = 'https://quant-1-a9oy.onrender.com/api';
 
 export const fetchModelPredictions = async (marketData) => {
     try {
